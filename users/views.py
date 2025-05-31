9a4dcbf8bfb6915e018949528343d34e87537633
aqibcs <csaqibshah@gmail.com>
2025-05-31 08:57:18 +0100

    Add register and login view functions

--- users/views.py
+++ users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.forms import UserLoginForm, UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request,
+                f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                messages.success(request, f"Welcome back, {username}!")
+                return redirect('home')  # Change as per your home URL name
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'users/login.html', {'form': form})
