7c70923cb5dbca80c4bbda93baa6419bd7cd58ca
aqibcs <csaqibshah@gmail.com>
2025-06-01 12:03:24 +0100

    feat: add profile and dashboard

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from users.forms import UserLoginForm, UserRegisterForm
+from users.forms import ProfileUpdateForm, UserLoginForm, UserRegisterForm, UserUpdateForm
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -38,3 +39,26 @@ def login_view(request):
         form = UserLoginForm()
 
     return render(request, 'users/login.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+        context = {'u_form': u_form, 'p_form': p_form}
+        return render(request, 'users/profile.html', context)
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
